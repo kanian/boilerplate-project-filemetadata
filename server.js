f50f587aaf6ed93b3524c8561c7d3aafc7a50b05
Patrick Assoa Adou <kanian77@gmail.com>
2019-12-02 17:14:30 +0000

    feat: :star: Add Upload File

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 'use strict';
 
-var express = require('express');
-var cors = require('cors');
+const express = require('express');
+const cors = require('cors');
+const upload = require('./getMulter')
 
-// require and use "multer"...
+const app = express();
 
-var app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -14,8 +14,15 @@ app.get('/', function (req, res) {
      res.sendFile(process.cwd() + '/views/index.html');
   });
 
-app.get('/hello', function(req, res){
-  res.json({greetings: "Hello, API"});
+app.post('/api/fileanalyse', upload.single('upfile'), function(req, res){
+  const file = req.file
+  if (!file) {
+    const error = new Error('Please upload a file')
+    error.httpStatusCode = 400
+    return next(error)
+  }
+  const {originalname, size, mimetype} = file
+    res.send({name: originalname, size, type: mimetype})
 });
 
 app.listen(process.env.PORT || 3000, function () {
